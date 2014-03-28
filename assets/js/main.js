2681c702b573b485ac15f7d2136960d3a788a278
Marko Cupic <m.cupic@gmx.ch>
2014-03-28 11:08:21 +0100

    added new icons & patched set new password error

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,17 @@ window.addEvent('domready', function () {
     $$('.mod_login input[name=username]').addEvents({
         keyup: function () {
             this.value = this.value.toLowerCase();
+        },
+        blur: function () {
+            this.value = this.value.toLowerCase();
         }
     });
 
     $$('.mod_account_settings input[name=email]').addEvents({
         keyup: function () {
+            this.value = this.value.toLowerCase();
+        },
+        blur: function () {
             this.value = this.value.toLowerCase();
         }
     });
--- modules/AccountSettingsController.php
+++ modules/AccountSettingsController.php
@@ -25,118 +25,124 @@ namespace MCupic;
  */
 class AccountSettingsController extends \Frontend
 {
-    /**
-     * @var $objMainController
-     */
-    protected $objMainController;
+       /**
+        * @var $objMainController
+        */
+       protected $objMainController;
 
-    public function __construct($objMainController)
-    {
-        $this->objMainController = $objMainController;
-        $this->import('FrontendUser', 'User');
-        return parent::__construct();
-    }
+       public function __construct($objMainController)
+       {
+              $this->objMainController = $objMainController;
+              $this->import('FrontendUser', 'User');
+              return parent::__construct();
+       }
 
-    /**
-     * Generate the module
-     */
-    public function setTemplate($objTemplate)
-    {
-        global $objPage;
-        $this->import('FrontendUser', 'User');
-        $GLOBALS['TL_LANGUAGE'] = $objPage->language;
+       /**
+        * Generate the module
+        */
+       public function setTemplate($objTemplate)
+       {
+              global $objPage;
+              $this->import('FrontendUser', 'User');
+              $GLOBALS['TL_LANGUAGE'] = $objPage->language;
 
-        \System::loadLanguageFile('tl_member');
-        $this->loadDataContainer('tl_member');
+              \System::loadLanguageFile('tl_member');
+              $this->loadDataContainer('tl_member');
 
-        $objTemplate->fields = '';
+              $objTemplate->fields = '';
 
-        $doNotSubmit = false;
-        $row = 0;
+              $doNotSubmit = false;
+              $row = 0;
 
-        $blnModified = false;
-        $objMember = \MemberModel::findByPk($this->User->id);
+              $blnModified = false;
+              $objMember = \MemberModel::findByPk($this->User->id);
 
-        $arrFields = array('password', 'email');
+              $arrFields = array('password', 'email');
 
-        $temp = '';
-        // Build the form
-        foreach ($arrFields as $field) {
-            $arrData = & $GLOBALS['TL_DCA']['tl_member']['fields'][$field];
-            $strClass = $GLOBALS['TL_FFL'][$arrData['inputType']];
+              $temp = '';
+              // Build the form
+              foreach ($arrFields as $field)
+              {
+                     $arrData = & $GLOBALS['TL_DCA']['tl_member']['fields'][$field];
+                     $strClass = $GLOBALS['TL_FFL'][$arrData['inputType']];
 
-            $strGroup = $arrData['eval']['feGroup'];
+                     $strGroup = $arrData['eval']['feGroup'];
 
-            $arrData['eval']['required'] = true;
-            $arrData['eval']['tableless'] = false;
+                     $arrData['eval']['required'] = true;
+                     $arrData['eval']['tableless'] = false;
 
-            $varValue = $this->User->$field;
+                     $varValue = $this->User->$field;
 
-            $objWidget = new $strClass($strClass::getAttributesFromDca($arrData, $field, $varValue, '', '', $this));
-            $objWidget->tableless = true;
-            $objWidget->storeValues = true;
-            $objWidget->rowClass = 'row_' . $row . (($row == 0) ? ' row_first' : '') . ((($row % 2) == 0) ? ' even' : ' odd');
+                     $objWidget = new $strClass($strClass::getAttributesFromDca($arrData, $field, $varValue, '', '', $this));
+                     $objWidget->tableless = true;
+                     $objWidget->storeValues = true;
+                     $objWidget->rowClass = 'row_' . $row . (($row == 0) ? ' row_first' : '') . ((($row % 2) == 0) ? ' even' : ' odd');
 
-            // Increase the row count if it is a password field
-            if ($objWidget instanceof \FormPassword) {
-                ++$row;
-                $objWidget->rowClassConfirm = 'row_' . $row . ((($row % 2) == 0) ? ' even' : ' odd');
-            }
+                     // Increase the row count if it is a password field
+                     if ($objWidget instanceof \FormPassword)
+                     {
+                            ++$row;
+                            $objWidget->rowClassConfirm = 'row_' . $row . ((($row % 2) == 0) ? ' even' : ' odd');
+                     }
 
-            // Validate the form data
-            if (\Input::post('FORM_SUBMIT') == 'tl_member_account_settings') {
-                $objWidget->validate();
-                $varValue = $objWidget->value;
+                     // Validate the form data
+                     if (\Input::post('FORM_SUBMIT') == 'tl_member_account_settings')
+                     {
+                            $objWidget->validate();
+                            $varValue = $objWidget->value;
 
-                // Do not submit the field if there are errors
-                if ($objWidget->hasErrors()) {
-                    $doNotSubmit = true;
-                } elseif ($objWidget->submitInput()) {
-                    // Store the form data
-                    $_SESSION['FORM_DATA'][$field] = $varValue;
+                            // Do not submit the field if there are errors
+                            if ($objWidget->hasErrors())
+                            {
+                                   $doNotSubmit = true;
+                            }
+                            elseif ($objWidget->submitInput())
+                            {
+                                   // Store the form data
+                                   $_SESSION['FORM_DATA'][$field] = $varValue;
 
-                    // Set the correct empty value (see #6284, #6373)
-                    if ($varValue === '') {
-                        $varValue = $objWidget->getEmptyValue();
-                    }
+                                   // Set the correct empty value (see #6284, #6373)
+                                   if ($varValue === '')
+                                   {
+                                          $varValue = $objWidget->getEmptyValue();
+                                   }
 
-                    // Set the new value
-                    $this->User->$field = $varValue;
+                                   // Set the new value
+                                   $this->User->$field = $varValue;
 
-                    // Set the new field in the member model
-                    $blnModified = true;
-                    $objMember->$field = $varValue;
-                }
-            }
-
-
-            $temp .= $objWidget->parse();
-
-            $objTemplate->fields .= $temp;
-            $arrFields[$strGroup][$field] .= $temp;
-            ++$row;
-        }
-
-        // Save the model
-        if ($blnModified) {
-            $objMember->save();
-            $objTemplate->submitted = true;
-        }
-
-        $objTemplate->fields = $temp;
-        $objTemplate->backLink = $this->generateFrontendUrl($objPage->row(), '/do/menu');
-
-        $objTemplate->hasError = $doNotSubmit;
-
-        $objTemplate->method = 'post';
-        $objTemplate->formId = 'tl_member_account_settings';
-        $objTemplate->slabel = specialchars($GLOBALS['TL_LANG']['MSC']['saveData']);
-        $objTemplate->action = $this->generateFrontendUrl($objPage->row(), '/do/account_settings') . setQueryString(array('act' => 'set_password'));
-        $objTemplate->enctype = 'application/x-www-form-urlencoded';
-        $objTemplate->rowLast = 'row_' . $row . ((($row % 2) == 0) ? ' even' : ' odd');
+                                   // Set the new field in the member model
+                                   $blnModified = true;
+                                   $objMember->$field = $varValue;
+                            }
+                     }
 
 
+                     $temp .= $objWidget->parse();
 
-        return $objTemplate;
-    }
+                     $objTemplate->fields .= $temp;
+                     $arrFields[$strGroup][$field] .= $temp;
+                     ++$row;
+              }
+
+              // Save the model
+              if ($blnModified && $doNotSubmit === false)
+              {
+                     $objMember->save();
+                     $objTemplate->submitted = true;
+              }
+
+              $objTemplate->fields = $temp;
+              $objTemplate->backLink = $this->generateFrontendUrl($objPage->row(), '/do/menu');
+
+              $objTemplate->hasError = $doNotSubmit;
+
+              $objTemplate->method = 'post';
+              $objTemplate->formId = 'tl_member_account_settings';
+              $objTemplate->slabel = specialchars($GLOBALS['TL_LANG']['MSC']['saveData']);
+              $objTemplate->action = $this->generateFrontendUrl($objPage->row(), '/do/account_settings') . setQueryString(array('act' => 'set_password'));
+              $objTemplate->enctype = 'application/x-www-form-urlencoded';
+              $objTemplate->rowLast = 'row_' . $row . ((($row % 2) == 0) ? ' even' : ' odd');
+
+              return $objTemplate;
+       }
 }
